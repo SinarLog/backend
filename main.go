57df9085a7b259f7aa54ce4dadc9c105f69295d1
fadliasyhariStar <fadli.asyhari@starcapital.co.id>
2023-08-31 14:00:47 +0700

    add git exec in main

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"os/exec"
 
 	"sinarlog.com/cmd/app"
 	"sinarlog.com/config"
@@ -14,6 +15,17 @@ func main() {
 			log.Fatalf("unable to set GO_ENV to DEVELOPMENT: %s\n", err)
 		}
 	}
+
+	gitCommitCmd := exec.Command("git", "rev-parse", "HEAD")
+	gitCommit, err := gitCommitCmd.Output()
+	if err != nil {
+		log.Fatalf("error running git rev-parse: %s\n", err)
+	}
+	gitCommitStr := string(gitCommit)
+	gitCommitStr = gitCommitStr[:len(gitCommitStr)-1]
+
+	os.Setenv("GIT_COMMIT", gitCommitStr)
+
 	cfg := config.GetConfig()
 
 	app.Run(cfg)
